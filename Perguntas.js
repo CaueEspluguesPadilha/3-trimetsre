6a2d45114cc0ce3bb4f20403df63045024dceae9
CaueEspluguesPadilha <caue.padilha@escola.pr.gov.br>
2024-10-30 11:04:50 -0300

    Update Perguntas.js

--- Perguntas.js
+++ Perguntas.js
@@ -5,8 +5,26 @@
 //chamada
 
 criacartao(
-    'Programação'
-    'o que é javaScript?'
+    'Programação',
+    'o que é javaScript?',
     'O JavaScript é uma lnguagem de programação'
 
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
